606ad0319e238a0b0ade8e8be1810af4e6fc27f1
Matt Laux <matthew.laux@gmail.com>
2016-12-22 15:30:08 -0600

    break repeating key xor, not quite working yet

--- cryptopals/set1.py
+++ cryptopals/set1.py
@@ -17,21 +17,24 @@ def fixed_xor(a, b):
 
 
 def break_single_byte_xor(a):
+    return _break_single_byte_xor(bytes.fromhex(a))
+
+
+def _break_single_byte_xor(a):
     letters = b'zqxjkvbpygfwmucldrhsnioate'
-    a = bytes.fromhex(a)
     max_score = 0
     best_result = None
-    for possible_key in range(1, 256):
+    best_key = 0
+    for possible_key in range(0, 256):
         fill = itertools.repeat(possible_key, len(a))
         result = bytearray((x ^ y for (x, y) in zip(a, fill)))
-        score = 0
-        for num in result:
-            score += letters.find(num)
+        score = sum(letters.find(num) for num in result)
         if score > max_score:
             max_score = score
             best_result = result
+            best_key = possible_key
 
-    return bytes(best_result), max_score
+    return bytes(best_result), max_score, best_key
 
 
 def detect_single_byte_xor(file):
@@ -39,7 +42,7 @@ def detect_single_byte_xor(file):
     best_result = None
     for line in open(file):
         line = line[:-1]
-        result, score = break_single_byte_xor(line)
+        result, score, _ = break_single_byte_xor(line)
         if score > max_score:
             max_score = score
             best_result = result
@@ -53,5 +56,33 @@ def repeating_key_xor(a, key):
     return binascii.hexlify(result)
 
 
-def break_repeating_key_xor(file):
-    return None
+def edit_distance(a, b):
+    return sum((bin(x ^ y).count('1') for (x, y) in zip(a, b)))
+
+
+def break_repeating_key_xor(file_name):
+    with open(file_name) as file:
+        contents = base64.b64decode(file.read())
+    best_size_score = 99999
+    best_size = 0
+    for keysize in range(2, 40):
+        pt1 = contents[0:keysize]
+        pt2 = contents[keysize:2*keysize]
+        size_score = edit_distance(pt1, pt2) / keysize
+        if size_score < best_size_score:
+            best_size_score = size_score
+            best_size = keysize
+    print('keysize is probably', best_size, best_size_score)
+    blocks = [contents[k:k+best_size] for k in range(0, len(contents), best_size)]
+    transpose = itertools.zip_longest(*blocks, fillvalue=0)
+    key = ''
+    decrypted_blocks = []
+    for block in transpose:
+        result, score, keypart = _break_single_byte_xor(block)
+        key += str(chr(keypart))
+        print(key, score, result)
+        decrypted_blocks.append(result)
+
+    val = b''.join(bytes(n) for n in zip(*decrypted_blocks))
+    print(val)
+    return bytes(val)
